927ad02c9978601f7bcc169320a3780851cd41e8
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-08-03 14:32:22 +0200

    Update files to redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import ContactForm from './Form';
+import ContactList from './ContactsList';
+import Filter from './Filter';
+import { Block } from './App.styled';
+import { GlobalStyles } from '../utils/GlobalStyles';
+// import { useSelector } from 'react-redux';
+// import { getContacts} from '../redux/selectors';
+
+// const LS_KEY = 'contacts-list';
+
 export const App = () => {
+  // const contacts = useSelector(getContacts);
+
+
+  // useEffect(() => {
+  //   const savedContacts = localStorage.getItem(LS_KEY);
+  //   if (savedContacts) {
+  //     setContacts(() => {
+  //       return JSON.parse(savedContacts);
+  //     });
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Block>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter/>
+      <ContactList/>
+      <GlobalStyles />
+    </Block>
   );
 };
---
+++ src/redux/selectors.js
@@ -0,0 +1,4 @@
+// функції виду export const getTasks = state => state.tasks
+export const getContacts = state => state.contacts
+
+export const getFilter = state => state.filter
