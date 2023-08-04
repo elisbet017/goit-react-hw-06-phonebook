7bc788d64057b6cf01df778bb84e18d68d992621
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-08-04 09:55:22 +0200

    Add persist with bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,8 @@ import ContactList from './ContactsList';
 import Filter from './Filter';
 import { Block } from './App.styled';
 import { GlobalStyles } from '../utils/GlobalStyles';
-// import { useSelector } from 'react-redux';
-// import { getContacts} from '../redux/selectors';
-
-// const LS_KEY = 'contacts-list';
 
 export const App = () => {
-  // const contacts = useSelector(getContacts);
-
-
-  // useEffect(() => {
-  //   const savedContacts = localStorage.getItem(LS_KEY);
-  //   if (savedContacts) {
-  //     setContacts(() => {
-  //       return JSON.parse(savedContacts);
-  //     });
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <Block>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,60 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactReducer,
+    contacts: persistedContactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
+// must
+
+// {
+//   contacts: [],
+//   filter: '',
+//   _persist: {
+//       version: -1,
+//       rehydrated: true
+//   }
+// }
+
+// my
+
+// {
+//   contacts: {
+//     _persist: {
+//       version: -1,
+//       rehydrated: true
+//     }
+//   },
+//   filter: ''
+// }
